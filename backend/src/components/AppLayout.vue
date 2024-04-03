b5e3dc3480cabc7cf457d50190bb5ecbe960dd32
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-03 18:51:00 +1000

    Uodates

---
+++ backend/src/components/AppLayout.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div class="flex min-h-full">
+
+    <!-- Side Bar -->
+    <div class="w-[200px] bg-blue-700">
+      Side bar
+    </div>
+    <!-- End Side Bar -->
+
+    <div class="flex-1">
+      <header class="h-8 shadow bg-white">
+        Header
+      </header>
+
+      <!-- Content -->
+      <main>
+        <router-view></router-view>
+      </main>
+      <!-- End Content -->
+
+    </div>
+
+
+  </div>
+
+
+</template>
+
+
+<script setup>
+
+const {title} =  defineProps({
+    title: String
+})
+
+</script>
+
+<style scoped>
+  
+</style>
--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,9 +1,22 @@
 import {createRouter, createWebHistory} from 'vue-router';
+import AppLayout from '../components/AppLayout.vue'
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import RequestPassword from '../views/RequestPassword.vue'
 import ResetPassword from '../views/ResetPassword.vue'
 const routes = [
+    {
+        path: '/app',
+        name: 'app',
+        component: AppLayout,
+        children: [
+            {
+                path: 'dashboard',
+                name: 'app.dashboard',
+                component: Dashboard
+            }
+        ]
+    },
     {
         path: '/dashboard',
         name: 'dashboard',

12cd7896a4486a4d2e20adff2dc9b80602d02c24
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-04 19:36:34 +1000

    Spelling

--- backend/src/views/NotFound.vue
+++ backend/src/views/NotFound.vue
@@ -4,7 +4,7 @@
             <div class="text-indigo-600 text-5xl px-8 font-bold border-r pb-8 leading-10">404</div>
         <div class="px-8">
             <h1 class="text-5xl font-bold mb-2">Page Not Found</h1>
-            <p class="text-gray-400 mb-10">Please check the url in the address bar and try again</p>
+            <p class="text-gray-400 mb-10">Please check the url in the address bar and try again!</p>
             <div>
                 <router-link :to="{name: 'app.dashboard'}" 
                     class="rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go Back Home</router-link>

86660a37aaa712a016f3371c34a8d71c65294551
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-06 23:07:41 +1000

    This branch is done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin' => \App\Http\Middleware\Admin::class
-        ])
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

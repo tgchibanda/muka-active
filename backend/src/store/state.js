8c1a9dbe6546d9f5ec5800ea5d2932fde43efce3
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-08 01:13:37 +1000

    Done

--- backend/src/store/state.js
+++ backend/src/store/state.js
@@ -1,8 +1,12 @@
-const store = {
+const state = {
     user: {
       token: sessionStorage.getItem('TOKEN'),
       data: {}
+    },
+    products: {
+      loading: false,
+      data: [],
     }
 };
 
-export default store
+export default state

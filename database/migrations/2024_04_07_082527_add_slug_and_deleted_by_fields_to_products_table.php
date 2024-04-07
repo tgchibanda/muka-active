a85e8b548180c924aab1e3450d6c155d11e175cd
Takunda Geraldino Chibanda <tgchibanda@gmail.com>
2024-04-08 00:17:26 +1000

    Done again (#11)

---
+++ database/migrations/2024_04_07_082527_add_slug_and_deleted_by_fields_to_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('slug', 2000);
+            $table->foreignIdFor(User::class, 'deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('slug');
+            $table->dropForeign(['deleted_by']);
+        });
+    }
+};

3f98cf1cdba44bd3b00cf9cfb562e516ed7c4b91
Takunda Geraldino Chibanda <tgchibanda@gmail.com>
2024-04-09 00:50:16 +1000

    Done (#19)

--- backend/src/views/Products/Products.vue
+++ backend/src/views/Products/Products.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="flex items-center justify-between mb-3">
   <h1 class="text-3xl font-semibold">Products</h1>
-  <button class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  <button type="submit"
+          @click="showPoductModal"
+          class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
   >
     Add new Product
   </button>
 </div>
+<ProductModal v-model="showModal"/>
 <ProductsTable />
 </template>
 
 <script setup>
 import ProductsTable from "./ProductsTable.vue";
+import ProductModal from "./ProductModal.vue";
+import {ref} from "vue";
 
+const showModal = ref(false)
+
+function showPoductModal(){
+  showModal.value = true
+}
 </script>
 
 <style scoped>

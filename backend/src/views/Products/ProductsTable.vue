5aea009dac2e85ec9d4e82494bf713cc55d0fff8
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-08 20:44:46 +1000

    Done

--- backend/src/views/Products/ProductsTable.vue
+++ backend/src/views/Products/ProductsTable.vue
@@ -1,11 +1,4 @@
 <template>
-  <div class="flex items-center justify-between mb-3">
-  <h1 class="text-3xl font-semibold">Products</h1>
-  <button class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-  >
-    Add new Product
-  </button>
-</div>
 <div class="bg-white p-4 rounded-lg shadow">
   <div class="flex justify-between border-b-2 pb-3">
     <div class="flex items-center">
@@ -104,10 +97,10 @@
 
 <script setup>
 import {computed, onMounted, ref} from 'vue';
-import store from "../store/index.js";
-import Spinner from "../components/core/Spinner.vue";
-import TableHeaderCell from "../components/core/Table/TableHeaderCell.vue";
-import {PRODUCTS_PER_PAGE} from "../constants.js";
+import store from "../../store/index.js";
+import Spinner from "../../components/core/Spinner.vue";
+import TableHeaderCell from "../../components/core/Table/TableHeaderCell.vue";
+import {PRODUCTS_PER_PAGE} from "../../constants.js";
 
 const perPage = ref(PRODUCTS_PER_PAGE)
 const search = ref ('')

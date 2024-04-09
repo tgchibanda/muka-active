342d68e34be2fbb22087947644433c3f7a794f03
Takunda Geraldino Chibanda <tgchibanda@gmail.com>
2024-04-09 22:04:34 +1000

    Done (#22)

--- backend/src/views/Products/ProductsTable.vue
+++ backend/src/views/Products/ProductsTable.vue
@@ -37,6 +37,9 @@
       <TableHeaderCell @click="sortProduct" class="border-b-2 p-2 text-left" field="updated_at" :sort-field="sortField" :sort-direction="sortDirection">
         Last Updated At
       </TableHeaderCell>
+      <TableHeaderCell class="border-b-2 p-2 text-left" field="actions">
+        Actions
+      </TableHeaderCell>
     </tr>
     </thead>
     <tbody v-if="products.loading">
@@ -54,10 +57,74 @@
         {{ product.title }}
       </td>
       <td class="border-b p-2">
-        {{ product.image }}
+        {{ product.price }}
       </td>
       <td class="border-b p-2 ">
         {{ product.updated_at }}
+      </td>
+      <td>
+        <Menu as ="div" class=""relative inline-block text-left>
+          <div>
+        <MenuButton
+          class="inline-flex items-center justify-center w-full rounded-full h-10 bg-black bg-opacity-0"
+        >
+          <DotsVerticalIcon
+            class="h-5 w-5 text-indigo-500"
+            aria-hidden="true"
+          />
+        </MenuButton>
+      </div>
+
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-in"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+      <MenuItems
+          class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+        >
+        <div class="px-1 py-1">
+
+          <MenuItem v-slot="{ active }">
+
+            <button
+              :class="[
+                active ? 'bg-indigo-600' : 'text-gray-900',
+                'group flex w-full items-center rounded-md px-2 py-2 text-sm']">
+                <PencilIcon
+                  :active="active"
+                  class="mr-2 h-5 w-5 text-indigo-400"
+                  aria-hidden="true"
+                  />
+                  Edit
+            </button>
+
+          </MenuItem>
+
+          <MenuItem v-slot="{ active }">
+
+            <button
+              :class="[
+                active ? 'bg-indigo-600' : 'text-gray-900',
+                'group flex w-full items-center rounded-md px-2 py-2 text-sm']"
+                @click="deleteProduct(product)">
+                <TrashIcon
+                  :active="active"
+                  class="mr-2 h-5 w-5 text-indigo-400"
+                  aria-hidden="true"
+                  />
+                  Delete
+            </button>
+
+          </MenuItem>
+        </div>
+      </MenuItems>
+    
+    </transition>
+        </Menu>
       </td>
     </tr>
     </tbody>
@@ -101,6 +168,8 @@ import store from "../../store/index.js";
 import Spinner from "../../components/core/Spinner.vue";
 import TableHeaderCell from "../../components/core/Table/TableHeaderCell.vue";
 import {PRODUCTS_PER_PAGE} from "../../constants.js";
+import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue';
+import {DotsVerticalIcon, PencilIcon, TrashIcon} from '@heroicons/vue/outline';
 
 const perPage = ref(PRODUCTS_PER_PAGE)
 const search = ref ('')
@@ -141,6 +210,17 @@ function sortProduct(field){
   }
 
   getProducts();
+}
+
+function deleteProduct(product) {
+  if(!confirm('Are you sure you want to delete the product?')){
+    return
+  }
+  store.dispatch('deleteProduct', product.id)
+  .then(res => {
+    // TODO Show notification
+    store.dispatch('getProducts')
+  })
 }
 </script>
 

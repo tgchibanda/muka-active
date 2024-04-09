996a823aa8ade1c1a6ed4c05a35ecb9f5bffdffb
Takunda Geraldino Chibanda <tgchibanda@gmail.com>
2024-04-09 19:29:04 +1000

    Done (#21)

--- backend/src/store/actions.js
+++ backend/src/store/actions.js
@@ -1,44 +1,82 @@
-import axiosClient from  "../axios";
+import axiosClient from "../axios";
 
-export function getUser({commit}){
-    return axiosClient.get('/user')
-    .then(response => {
-        commit('setUser', response.data)
-        return response
-    })
-}
-
-export function login({commit}, data){
-    return axiosClient.post('/login', data)
+export function getUser({commit}, data) {
+  return axiosClient.get('/user', data)
     .then(({data}) => {
-        commit('setUser', data);
-        commit('setToken', data.token)
-        return data;
+      commit('setUser', data);
+      return data;
     })
 }
 
-export function logout({commit}){
-    return axiosClient.post('/logout')
+export function login({commit}, data) {
+  return axiosClient.post('/login', data)
+    .then(({data}) => {
+      commit('setUser', data.user);
+      commit('setToken', data.token)
+      return data;
+    })
+}
+
+export function logout({commit}) {
+  return axiosClient.post('/logout')
     .then((response) => {
-        commit('setToken', null)
-        return response;
+      commit('setToken', null)
+
+      return response;
     })
 }
 
-export function getProducts({commit}, {url = null, search = '', perPage = 10, sort_field, sort_direction}){
-    commit('setProducts', [true])
-    url = url || '/product';
-    return axiosClient.get(url,{
-        params: {
-            search, 
-            per_page: perPage,
-            sort_field,
-            sort_direction}
-    })
-    .then((res) => {
-        commit('setProducts', [false, res.data])
+export function getProducts({commit, state}, {url = null, search = '', per_page, sort_field, sort_direction} = {}) {
+  commit('setProducts', [true])
+  url = url || '/products'
+  const params = {
+    per_page: state.products.limit,
+  }
+  return axiosClient.get(url, {
+    params: {
+      ...params,
+      search, per_page, sort_field, sort_direction
+    }
+  })
+    .then((response) => {
+      commit('setProducts', [false, response.data])
     })
     .catch(() => {
-        commit('setProducts', [false])
+      commit('setProducts', [false])
     })
+}
+
+export function getProduct({commit}, id) {
+  return axiosClient.get(`/products/${id}`)
+}
+
+export function  createProduct({commit}, product) {
+  if (product.image instanceof File) {
+    const form = new FormData();
+    form.append('title', product.title);
+    form.append('image', product.image);
+    form.append('description', product.description);
+    form.append('price', product.price);
+    product = form;
+  }
+  return axiosClient.post('/products', product)
+}
+
+export function updateProduct({commit}, product) {
+  const id = product.id
+  if (product.image instanceof File) {
+    const form = new FormData();
+    form.append('id', product.id);
+    form.append('title', product.title);
+    form.append('image', product.image);
+    form.append('description', product.description);
+    form.append('price', product.price);
+    form.append('_method', 'PUT');
+    product = form;
+  }
+  return axiosClient.post(`/products/${id}`, product)
+}
+
+export function deleteProduct({commit}, id) {
+  return axiosClient.delete(`/products/${id}`)
 }
--- backend/src/views/Products/ProductsTable.vue
+++ backend/src/views/Products/ProductsTable.vue
@@ -54,7 +54,7 @@
         {{ product.title }}
       </td>
       <td class="border-b p-2">
-        {{ product.price }}
+        {{ product.image }}
       </td>
       <td class="border-b p-2 ">
         {{ product.updated_at }}

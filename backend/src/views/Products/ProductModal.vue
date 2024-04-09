ee209e3f9b6423bb82676d6bbd8d36cf348baa69
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-09 12:51:54 +1000

    Done

--- backend/src/views/Products/ProductModal.vue
+++ backend/src/views/Products/ProductModal.vue
@@ -27,30 +27,54 @@
             leave-to="opacity-0 scale-95"
           >
             <DialogPanel
-              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
-            >
-              <DialogTitle
-                as="h3"
-                class="text-lg font-medium leading-6 text-gray-900"
-              >
-                Payment successful
-              </DialogTitle>
-              <div class="mt-2">
-                <p class="text-sm text-gray-500">
-                  Your payment has been successfully submitted. We’ve sent you
-                  an email with all of the details of your order.
-                </p>
-              </div>
-
-              <div class="mt-4">
+              class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-[700px] sm:w-full">
+              
+              <Spinner v-if="loading"
+                       class="absolute left-0 top-0 bg-white right-0 bottom-0 flex items-center justify-center"/>
+              <header class="py-3 px-4 flex justify-between items-center">
+                <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900">
+                  {{ product.id ? `Update product: "${props.product.title}"` : 'Create new Product' }}
+                </DialogTitle>
                 <button
-                  type="button"
-                  class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                  @click="closeModal"
+                  @click="closeModal()"
+                  class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]"
                 >
-                  Got it, thanks!
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
                 </button>
-              </div>
+              </header>
+              <form @submit.prevent="onSubmit">
+                <div class="bg-white px-4 pt-5 pb-4">
+                  <CustomInput class="mb-2" v-model="product.title" label="Product Title"/>
+                  <CustomInput type="file" class="mb-2" label="Product Image" @change="file => product.image = file"/>
+                  <CustomInput type="textarea" class="mb-2" v-model="product.description" label="Description"/>
+                  <CustomInput type="number" class="mb-2" v-model="product.price" label="Price" prepend="$"/>
+                </div>
+                <footer class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button type="submit"
+                          class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-3">
+                    Submit
+                  </button>
+                  <button type="button"
+                          class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                          @click="closeModal" ref="cancelButtonRef">
+                    Cancel
+                  </button>
+                </footer>
+              </form>
+
             </DialogPanel>
           </TransitionChild>
         </div>
@@ -60,7 +84,7 @@
 </template>
 
 <script setup>
-import {ref, computed} from 'vue'
+import {ref, computed, onUpdated} from 'vue'
 import {
   TransitionRoot,
   TransitionChild,
@@ -68,18 +92,78 @@ import {
   DialogPanel,
   DialogTitle,
 } from '@headlessui/vue'
+import store from '../../store/index.js'
+import CustomInput from '../../components/core/CustomInput.vue'
+import Spinner from "../../components/core/Spinner.vue";
+
+
+const loading = ref(false);
 
 const props = defineProps({
-  modelValue : Boolean
+  modelValue : Boolean,
+  product: {
+    requred: true,
+    type: Object,
+  }
 })
-const emit = defineEmits(['update:modelValue'])
+
+const product =ref({
+  id: props.product.id,
+  title: props.product.title,
+  image: props.product.image,
+  description: props.product.description,
+  price: props.product.price,
+})
+
+const emit = defineEmits(['update:modelValue', 'close'])
+
 const show = computed({
   get: () => props.modelValue,
   set: (value) => emit('update:modelValue', value)
 })
 
+onUpdated(()=>{
+  product.value = {
+    id: props.product.id,
+    title: props.product.title,
+    image: props.product.image,
+    description: props.product.description,
+    price: props.product.price,
+  }
+})
+
 function closeModal() {
   show.value = false
+  emit('close')
+}
+
+function onSubmit() {
+  loading.value = true
+  if (product.value.id) {
+    store.dispatch('updateProduct', product.value)
+      .then(response => {
+        loading.value = false;
+        if (response.status === 200) {
+          // TODO show notification
+          store.dispatch('getProducts')
+          closeModal()
+        }
+      })
+  } else {
+    store.dispatch('createProduct', product.value)
+      .then(response => {
+        loading.value = false;
+        if (response.status === 201) {
+          // TODO show notification
+          store.dispatch('getProducts')
+          closeModal()
+        }
+      })
+      .catch(err => {
+        loading.value = false;
+        debugger;
+      })
+  }
 }
 
 </script>

7357a1b95e77f753c54391f85df2b02566b50246
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-11 15:34:56 +1000

    Fix modal css by deleting the anomation class in the tr

--- backend/src/views/Products/ProductsTable.vue
+++ backend/src/views/Products/ProductsTable.vue
@@ -48,7 +48,7 @@
       </tr>
     </tbody>
     <tbody v-else>
-    <tr v-for="(product, index) of products.data" class="animate-fade-in-down" :style="{'animation-delay': `${index*0.05}s`}">
+    <tr v-for="(product, index) of products.data" class="animate-fade-in" :style="{'animation-delay': `${index*0.05}s`}">
       <td class="border-b p-2 ">{{ product.id }}</td>
       <td class="border-b p-2 ">
         <img class="w-16" :src="product.image_url" :alt="product.title">

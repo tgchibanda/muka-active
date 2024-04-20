0a5c961df1cbd1cfe1466a23dc27ec00a100eb95
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-21 02:05:31 +1000

    Done

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,7 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-emerald-600 py-3 px-4 text-white rounded']) }}>
         {{ $status }}
     </div>
 @endif

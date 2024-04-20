860db6b369b66e51f792e9d62232c2119c4781cf
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-20 19:52:22 +1000

    Done

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,18 @@
+@props(['disabled' => false, 'errors', 'label' => false])
+
+<?php
+    $errorClasses = 'border-red-600 focus:border-red-600 ring-1 ring-red-600 focus:ring-red-600';
+    $defaultClasses = '';
+    $successClasses = 'border-emerald-500 focus:border-emerald-500 ring-1 ring-emerald-600 focus:ring-emerald-600';
+?>
+
+@if($label)
+    <label>{{$label}}</label>
+@endif
+<input {{ $disabled ? 'disabled' : ''}} {!! $attributes -> merge ([
+    'class' => 'border-gray-300 focus:border-purple-500 focus:outline-none focus:ring-purple-500 rounded-md w-full ' . 
+    ($errors->has($attributes['name']) ? $errorClasses : (old($attributes['name']) ? $successClasses : $defaultClasses))
+    ]) !!}>
+@error($attributes['name'])
+<small>{{$message}}</small>
+@enderror

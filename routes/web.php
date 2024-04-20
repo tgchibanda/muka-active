9a5fa7dd7d4a9297ba336c1ca75b50e4a0e09060
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-20 23:50:44 +1000

    Done

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [\App\Http\Controllers\ProductController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');

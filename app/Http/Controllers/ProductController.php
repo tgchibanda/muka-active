865f2cabfbc687dbb54be014b1060359ab149754
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-21 00:07:48 +1000

    Done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index(){
-        $products = Product::query()->orderBy('updated_at', 'desc')->paginate(5);
+        $products = Product::query()->orderBy('updated_at', 'desc')->paginate(4);
         return view('product.index' , [
             'products' => $products
         ]);
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -14,11 +14,11 @@
           })"
           class="border border-1 border-gray-200 rounded-md hover:border-purple-600 transition-colors bg-white"
         >
-          <a href="/src/product.html" class="block overflow-hidden">
+          <a href="/src/product.html" class="block overflow-hidden aspect-w-7 aspect-h-7">
             <img
               src="{{ $product->image }}"
               alt=""
-              class="rounded-lg hover:scale-105 hover:rotate-1 transition-transform"
+              class="rounded-lg hover:scale-105 hover:rotate-1 transition-transform object-cover"
             />
           </a>
           <div class="p-4">
@@ -57,4 +57,6 @@
         </div>
         @endforeach
     </div>
+
+    {{$products->links()}}
 </x-app-layout>

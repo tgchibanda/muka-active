52cf7f62c2da26352da23faa9dc55871179f83a1
Takunda Geraldino Chibanda <tgchibanda@gmail.com>
2024-04-29 15:52:55 +1000

    Fixed all errors with dev branch (#43)

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use App\Http\Helpers\Cart;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -28,7 +29,9 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        Cart::moveCartItemsIntoDb();
+
+        return redirect()->intended(route('home', absolute: false));
     }
 
     /**

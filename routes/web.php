ae98c2dfbe061f14a21dcc764fae520e9cf85757
Takunda Chibanda <tgchibanda@gmail.com>
2024-04-30 19:52:01 +1000

    Section complete

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +30,8 @@ Route::middleware(['guestOrVerified'])->group(function () {
 
 Route::middleware(['auth', 'verified'])->group(function() {
     Route::get('/profile', [ProfileController::class, 'view'])->name('profile');
+    Route::post('/profile', [ProfileController::class, 'store'])->name('profile.update');
+    Route::post('/profile/password-update', [ProfileController::class, 'passwordUpdate'])->name('profile_password.update');
 });
 
 require __DIR__ . '/auth.php';

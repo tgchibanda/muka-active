3787b1a8b340bdbcd8fa4625ea7f4c656838fa0f
Takunda Chibanda <tgchibanda@gmail.com>
2024-05-01 18:11:05 +1000

    Payments now handled including creating orders

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Helpers\Cart;
+use App\Enums\OrderStatus;
+use App\Enums\PaymentStatus;
+use App\Models\Order;
+use App\Models\Payment;
 
 class CheckoutController extends Controller
 {
     
     public function checkout(Request $request)
     {
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+
         list($products, $cartItems) = Cart::getProductsAndCartItems();
 
         $lineItems = [];
+        $totalPrice = 0;
         foreach ($products as $product) {
+            $quantity = $cartItems[$product->id]['quantity'];
+            $totalPrice += $product->price * $quantity;
             $lineItems[] = [
                 'price_data' => [
                     'currency' => 'usd',
@@ -23,7 +33,7 @@ class CheckoutController extends Controller
                     ],
                     'unit_amount' => $product->price * 100,
                   ],
-                  'quantity' => $cartItems[$product->id]['quantity'],
+                  'quantity' => $quantity,
                 ];
         }
 
@@ -37,6 +47,28 @@ class CheckoutController extends Controller
             'customer_creation' => 'always',
           ]);
 
+          $orderData =[
+            'total_price' => $totalPrice,
+            'status' => OrderStatus::Unpaid,
+            'created_by' => $user->id,
+            'updated_by' => $user->id,
+          ];
+
+          $order = Order::create($orderData);
+
+          $paymentData =[
+            'order_id' => $order->id, // will use the just created id or the orders table
+            'amount' => $totalPrice,
+            'status' => PaymentStatus::Pending,
+            'updated_by' => $user->id,
+            'type' => 'cc',
+            'created_by' => $user->id,
+            'updated_by' => $user->id,
+            'session_id' => $checkout_session->id,
+          ]; 
+
+          Payment::create($paymentData);
+
           return redirect($checkout_session->url);
     }
 
@@ -44,13 +76,33 @@ class CheckoutController extends Controller
     {
         \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
         try {
-            $checkout_session = \Stripe\Checkout\Session::retrieve($request->get('session_id'));
+            $session_id = $request->get('session_id');
+            $checkout_session = \Stripe\Checkout\Session::retrieve($session_id);
+
             if(!$checkout_session){
                 return view('checkout.failure', compact('customer'));
             }
+
+            $payment = Payment::query()->where(['session_id' => $checkout_session->id, 'status' => PaymentStatus::Pending])->first();
+
+            if(!$payment){
+                return view('checkout.failure', compact('customer'));
+            }
+
+            $payment->status = PaymentStatus::Paid;
+            $payment->update();
+
+            $order = $payment->order;
+            $order->status = OrderStatus::Paid;
+            $order->update();
+
+
             $customer = \Stripe\Customer::retrieve($checkout_session->customer);
+            
             return view('checkout.success', compact('customer'));
+
         } catch (\Exception $e){
+            //dd("Ine exception ". $e);
             return view('checkout.failure');
         }
     }

8f830c0c6f035514a140ee8fa42b7b5272c2658f
Takunda Chibanda <tgchibanda@gmail.com>
2024-05-01 16:31:49 +1000

    Checkout sucess corrected

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,214 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\OrderStatus;
-use App\Enums\PaymentStatus;
-use App\Http\Helpers\Cart;
-use App\Models\CartItem;
-use App\Models\Order;
-use App\Models\OrderItem;
-use App\Models\Payment;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Http\Helpers\Cart;
 
 class CheckoutController extends Controller
 {
+    
     public function checkout(Request $request)
     {
-        /** @var \App\Models\User $user */
-        $user = $request->user();
+        list($products, $cartItems) = Cart::getProductsAndCartItems();
 
-        \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
-
-        [$products, $cartItems] = Cart::getProductsAndCartItems();
-
-        $orderItems = [];
         $lineItems = [];
-        $totalPrice = 0;
         foreach ($products as $product) {
-            $quantity = $cartItems[$product->id]['quantity'];
-            $totalPrice += $product->price * $quantity;
             $lineItems[] = [
                 'price_data' => [
                     'currency' => 'usd',
                     'product_data' => [
-                        'name' => $product->title,
-//                        'images' => [$product->image]
+                      'name' => $product->title,
+                      'images' => [$product->image],
                     ],
                     'unit_amount' => $product->price * 100,
-                ],
-                'quantity' => $quantity,
-            ];
-            $orderItems[] = [
-                'product_id' => $product->id,
-                'quantity' => $quantity,
-                'unit_price' => $product->price
-            ];
+                  ],
+                  'quantity' => $cartItems[$product->id]['quantity'],
+                ];
         }
-//        dd(route('checkout.failure', [], true));
 
-//        dd(route('checkout.success', [], true) . '?session_id={CHECKOUT_SESSION_ID}');
 
-        $session = \Stripe\Checkout\Session::create([
+        \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
+        $checkout_session = \Stripe\Checkout\Session::create([
             'line_items' => $lineItems,
             'mode' => 'payment',
             'success_url' => route('checkout.success', [], true) . '?session_id={CHECKOUT_SESSION_ID}',
             'cancel_url' => route('checkout.failure', [], true),
-        ]);
+            'customer_creation' => 'always',
+          ]);
 
-        // Create Order
-        $orderData = [
-            'total_price' => $totalPrice,
-            'status' => OrderStatus::Unpaid,
-            'created_by' => $user->id,
-            'updated_by' => $user->id,
-        ];
-        $order = Order::create($orderData);
-
-        // Create Order Items
-        foreach ($orderItems as $orderItem) {
-            $orderItem['order_id'] = $order->id;
-            OrderItem::create($orderItem);
-        }
-
-        // Create Payment
-        $paymentData = [
-            'order_id' => $order->id,
-            'amount' => $totalPrice,
-            'status' => PaymentStatus::Pending,
-            'type' => 'cc',
-            'created_by' => $user->id,
-            'updated_by' => $user->id,
-            'session_id' => $session->id
-        ];
-        Payment::create($paymentData);
-
-        CartItem::where(['user_id' => $user->id])->delete();
-
-        return redirect($session->url);
+          return redirect($checkout_session->url);
     }
 
     public function success(Request $request)
     {
-        /** @var \App\Models\User $user */
-        $user = $request->user();
         \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
-
         try {
-            $session_id = $request->get('session_id');
-            $session = \Stripe\Checkout\Session::retrieve($session_id);
-            if (!$session) {
-                return view('checkout.failure', ['message' => 'Invalid Session ID']);
+            $checkout_session = \Stripe\Checkout\Session::retrieve($request->get('session_id'));
+            if(!$checkout_session){
+                return view('checkout.failure', compact('customer'));
             }
-
-            $payment = Payment::query()
-                ->where(['session_id' => $session_id])
-                ->whereIn('status', [PaymentStatus::Pending, PaymentStatus::Paid])
-                ->first();
-            if (!$payment) {
-                throw new NotFoundHttpException();
-            }
-            if ($payment->status === PaymentStatus::Pending->value) {
-                $this->updateOrderAndSession($payment);
-            }
-            $customer = \Stripe\Customer::retrieve($session->customer);
+            $customer = \Stripe\Customer::retrieve($checkout_session->customer);
             return view('checkout.success', compact('customer'));
-        } catch (NotFoundHttpException $e) {
-            throw $e;
-        } catch (\Exception $e) {
-            return view('checkout.failure', ['message' => $e->getMessage()]);
+        } catch (\Exception $e){
+            return view('checkout.failure');
         }
     }
-
     public function failure(Request $request)
     {
-        return view('checkout.failure', ['message' => ""]);
-    }
-
-    public function checkoutOrder(Order $order, Request $request)
-    {
-        \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
-
-        $lineItems = [];
-        foreach ($order->items as $item) {
-            $lineItems[] = [
-                'price_data' => [
-                    'currency' => 'usd',
-                    'product_data' => [
-                        'name' => $item->product->title,
-//                        'images' => [$product->image]
-                    ],
-                    'unit_amount' => $item->unit_price * 100,
-                ],
-                'quantity' => $item->quantity,
-            ];
-        }
-
-        $session = \Stripe\Checkout\Session::create([
-            'line_items' => $lineItems,
-            'mode' => 'payment',
-            'success_url' => route('checkout.success', [], true) . '?session_id={CHECKOUT_SESSION_ID}',
-            'cancel_url' => route('checkout.failure', [], true),
-        ]);
-
-        $order->payment->session_id = $session->id;
-        $order->payment->save();
-
-
-        return redirect($session->url);
-    }
-
-    public function webhook()
-    {
-        \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
-
-        $endpoint_secret = 'whsec_9e29e6c3bcd5422dedd5cab8df9e924e14d5f36dc285c284e8f23d6beaf11c2c';
-
-        $payload = @file_get_contents('php://input');
-        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
-        $event = null;
-
-        try {
-            $event = \Stripe\Webhook::constructEvent(
-                $payload, $sig_header, $endpoint_secret
-            );
-        } catch (\UnexpectedValueException $e) {
-            // Invalid payload
-            return response('', 401);
-        } catch (\Stripe\Exception\SignatureVerificationException $e) {
-            // Invalid signature
-            return response('', 402);
-        }
-
-        // Handle the event
-        switch ($event->type) {
-            case 'checkout.session.completed':
-                $paymentIntent = $event->data->object;
-                $sessionId = $paymentIntent['id'];
-
-                $payment = Payment::query()
-                    ->where(['session_id' => $sessionId, 'status' => PaymentStatus::Pending])
-                    ->first();
-                if ($payment) {
-                    $this->updateOrderAndSession($payment);
-                }
-            // ... handle other event types
-            default:
-                echo 'Received unknown event type ' . $event->type;
-        }
-
-        return response('', 200);
-    }
-
-    private function updateOrderAndSession(Payment $payment)
-    {
-        $payment->status = PaymentStatus::Paid;
-        $payment->update();
-
-        $order = $payment->order;
-
-        $order->status = OrderStatus::Paid;
-        $order->update();
-
+        return view('checkout.failure', compact('customer'));
     }
 }

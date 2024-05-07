55df9825049107eab5808de85c35d75e8c66fb36
Takunda Chibanda <tgchibanda@gmail.com>
2024-05-07 23:42:06 +1000

    Halfway done

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,3 +1,7 @@
 export const PRODUCTS_PER_PAGE = 10;
 export const USERS_PER_PAGE = 10;
 export const CUSTOMERS_PER_PAGE = 10;
+export const CURRENCY = {
+    country : 'en-AU',
+    symbol : 'AUD'
+};

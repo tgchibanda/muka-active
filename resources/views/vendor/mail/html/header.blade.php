1365b9a0f11b7e38b61499035040b69ca6b726cc
Takunda Geraldino Chibanda <tgchibanda@gmail.com>
2024-05-12 15:13:36 +1000

    112 bug fixing (#71)
    * Updates
    
    * Updates
    
    * Icons and Logo
    
    * Navigation and email logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="../../../layouts/symbol_logo.jpg" class="logo" alt="Muka Logo">
+@if (trim($slot) === 'Muka-Active')
+<img src="../../../../../public/logo-black.png" class="logo" alt="Muka-Active Logo">
 @else
 {{ $slot }}
 @endif
